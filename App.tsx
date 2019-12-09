1497a8e70e1c1e60f702f5e12e071dc770df05d2
NikiSpassov <nikispassov@outlook.com>
2019-12-09 21:32:30 +0200

    Working camera

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import CameraExample from './src/components/CameraExample';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    // <View style={styles.container}>
+    //   <Text>Open up App.tsx to start working on your app.</Text>
+    // </View>
+    <CameraExample/>
   );
 }
 
